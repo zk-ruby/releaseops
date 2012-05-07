b708c511f95d26d6e6253e87816b9030854d867b
slyphon <slyphon@gmail.com>
2012-05-07 17:52:49 +0000

    add ReleaseOps.with_tmpdir

--- lib/releaseops.rb
+++ lib/releaseops.rb
@@ -9,8 +9,27 @@ module ReleaseOps
     end
   end
 
-  def self.gem_files
-    FileList['*zookeeper-*.gem']
+  # @option opts [String] :prefix ('releaseops') a string to prepend to the
+  #   directory name generated under Dir.tmpdir
+  #
+  # @option opts [true,false] :autoclean (true) if true, we remove the directory
+  #   if false, we only remove the directory if there was no exception
+  #
+  def self.with_tmpdir(opts={})
+    require 'tmpdir'
+    prefix = opts.fetch(:prefix, 'releaseops')
+
+    dir = File.join(Dir.tmpdir, "#{prefix}.#{rand(1_000_000)}_#{$$}_#{Time.now.strftime('%Y%m%d%H%M%S')}")
+
+    $stderr.puts "Using tmpdir: #{dir}"
+
+    FileUtils.mkdir_p(dir)
+
+    yield(dir).tap do
+      FileUtils.rm_rf(dir) unless opts[:autoclean]
+    end
+  ensure
+    FileUtils.rm_rf(dir) if opts[:autoclean]
   end
 end
 
