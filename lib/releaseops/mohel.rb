d2066a02657b1c6a6ab7b3ff5e99a659e416667c
slyphon <slyphon@gmail.com>
2012-05-07 15:33:45 +0000

    yeah, save this and abandon it

--- lib/releaseops/mohel.rb
+++ lib/releaseops/mohel.rb
@@ -1,10 +1,11 @@
 module ReleaseOps
+  require_libs(*%w[ mohel/git_utils ])
+
   # http://en.wikipedia.org/wiki/Mohel
   #
   # the mohel cuts our gems
   module Mohel
     class MohelError < StandardError; end
-
 
     class BuildTask < ::Rake::TaskLib
       include GitUtils
@@ -20,6 +21,7 @@ module ReleaseOps
       # which is the clean-room phase of the build
       attr_reader :phase_two_env
 
+      attr_accessor :tag
   
       def initialize(namespace=:mohel)
         @namespace = namespace
@@ -35,9 +37,12 @@ module ReleaseOps
       end
 
       private
-
         def phase_two?
           ENV['PHASE'] == '2'
+        end
+
+        def tag
+          ENV['TAG'] || raise "You must set TAG in the environment"
         end
         
         # the PACKAGE_DIR env var will be set in subordinate rake invocations
@@ -48,13 +53,14 @@ module ReleaseOps
 
         # runs the actual gem creation task in the "clean room" directory (phase 2)
         def run_gem_package_task
-          sh("rvm 1.8.7 do gem build #{@gemspec_name}")
+          sh "rvm 1.8.7 do gem build #{@gemspec_name}"
+          cp FileList["*.gem"], package_dir
+
         end
 
         def trace?
           Rake.application.options.trace
         end
-
   
         # preps the clean room build and execs the second phase of the build (phase 1)
         def do_clean_room_build_of_gem_at_tag
@@ -63,14 +69,21 @@ module ReleaseOps
           $stderr.puts "cloning repo at tag '#{tag}' to #{@temp_dir_path}"
           git_archive_tag(tag, @temp_dir_path)
           $stderr.puts "running phase two"
-          phase_two_env_args = phase_two_env.sort.map { |k,v| "#{k}='#{v}'" }.join(' ')
-          sh "cd #{@temp_dir_path} && rake #{trace? and '-t '}#{@namespace}:phase_two_gem['#{tag}'] PHASE=2 PACKAGE_DIR=#{package_dir} #{phase_two_env_args}"
-        end
 
+          cmd = "rake #{trace? and '-t '}#{@namespace}:phase_two_gem['#{tag}']"
+
+          env = phase_two_env.merge('PHASE' => 2, 'PACKAGE_DIR' => package_dir, 'TAG' => tag)
+
+          unless system(env, cmd, :chdir => @temp_dir_path)
+            raise "system(#{env.inspect}, #{cmd.inspect}, :chdir => #{@temp_dir_path.inspect}) failed" 
+          end
+        end
 
         def define
           raise "Couldn't find a gemspec and none specified" unless @gemspec_name
           raise "Specified gemspec doesn't exist" unless File.exists?(@gemspec_name)
+
+          directory package_dir
 
           namespace(@namespace) do
             task :do_git_fetch do
@@ -83,37 +96,33 @@ module ReleaseOps
             # this will only run if we've done the whole "clean-room" thing and
             # invoked rake as a subtask
             if phase_two?
-              task :phase_two_gem, [:tag] => :before_gem_package do |t,args|
+              task :phase_two_gem => :before_gem_package do |t,args|
                 $stderr.puts "[DEBUG] phase_two_gem args: #{args.inspect}"
-                ENV['TAG'] = self.tag = args.tag if args.tag
                 run_gem_package_task 
               end
             end
 
-            task :build_gem, [:tag] => :do_git_fetch do |t,args|
+            task :build_gem => [:do_git_fetch, package_dir] do |t,args|
               self.tag = args.tag if args.tag
               do_clean_room_build_of_gem_at_tag
               rm_rf @temp_dir_path
             end
 
-            desc %Q[
-              |create a gem. if argument :tag is specified, then use that as the tag and
-              |use the basename of the tag name as the gem version. If :tag is
-              |not given, build the gem for the latest tag in the repo. Then 
-              |push the created gem up to our rubygems repository.
-            ].margin
+#             desc %Q[
+#               |create a gem. if argument :tag is specified, then use that as the tag and
+#               |use the basename of the tag name as the gem version. If :tag is
+#               |not given, build the gem for the latest tag in the repo. Then 
+#               |push the created gem up to our rubygems repository.
+#             ].margin
 
-            task :publish_gem, [:tag] => :build_gem do |t,args|
-              self.tag = args.tag if args.tag
-              sh "gem push "
-            end
+#             task :publish_gem, [:tag] => :build_gem do |t,args|
+#               self.tag = args.tag if args.tag
+#               sh "gem push "
+#             end
           end
         end
     end
   end
 end
 
-ReleaseOps.require_libs(*%w[
-  mohel/git_utils
-])
 
